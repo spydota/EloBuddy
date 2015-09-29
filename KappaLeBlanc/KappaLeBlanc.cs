8dc7a440416e2b33851c9461f5395bb7c24338e5
Phandaros <phandarosgames@gmail.com>
2015-09-29 19:40:43 -0300

    toolazy

--- KappaLeBlanc/KappaLeBlanc.cs
+++ KappaLeBlanc/KappaLeBlanc.cs
@@ -6,25 +6,30 @@ using EloBuddy.SDK.Menu;
 using EloBuddy.SDK.Menu.Values;
 using EloBuddy.SDK.Rendering;
 using System;
-using System.Linq;
 using Color = System.Drawing.Color;
 namespace KappaLeBlanc
 {
     class Program
     {
-        public const string LeBlanc = "LeBlanc";
-        public static Menu Menu,
-            DrawMenu;
+        static void OnBuffGain(Obj_AI_Base sender, Obj_AI_BaseBuffGainEventArgs buff)
+        {
+            if (sender.IsMe && buff.Buff.Name == "Distortion")
+                Chat.Print("ichigolixo");
+        }
 
+        public const string Leblanc = "Leblanc";
+        public static Menu Menu,
+            DrawMenu,
+            ComboMenu,
+            HarassMenu;
         public static Spell.Targeted Q;
         public static Spell.Skillshot W;
         public static Spell.Skillshot E;
         public static Spell.Targeted R;
-
         public static AIHeroClient _Player
-            {
-                get { return ObjectManager.Player; }
-            }
+        {
+            get { return ObjectManager.Player; }
+        }
 
         static void Main(string[] args)
         {
@@ -35,45 +40,86 @@ namespace KappaLeBlanc
 
         private static void Game_OnStart(EventArgs args)
         {
-            if (Player.Instance.ChampionName != LeBlanc)
+            Obj_AI_Base.OnBuffGain += OnBuffGain;
+            if (Player.Instance.ChampionName == Leblanc)
+            {
+                Chat.Print("Leblanc loaded!");
+            }
+
+            else if (Player.Instance.ChampionName != Leblanc)
             {
                 Chat.Print("Champion not supported");
             }
 
-            if (Player.Instance.ChampionName == LeBlanc)
-            {
-                Chat.Print("Leblanc loaded!");
-            }
             Q = new Spell.Targeted(SpellSlot.Q, 700);
             W = new Spell.Skillshot(SpellSlot.W, 600, SkillShotType.Circular);
             E = new Spell.Skillshot(SpellSlot.E, 950, SkillShotType.Linear);
             R = new Spell.Targeted(SpellSlot.R, 700);
-            
+
 
 
             Menu = MainMenu.AddMenu("LeBlanc", "leblanc");
-            Menu.AddSeparator();
             Menu.AddLabel("Raise your Kappa");
 
             DrawMenu = Menu.AddSubMenu("Draw", "draw");
             DrawMenu.Add("drawDisable", new CheckBox("Disable draws", true));
+            DrawMenu.Add("drawQ", new CheckBox("Draw Q", false));
+            DrawMenu.Add("drawW", new CheckBox("Draw W", false));
+            DrawMenu.Add("drawE", new CheckBox("Draw E", false));
+        
+            ComboMenu = Menu.AddSubMenu("Combo", "lbcombo");
+            ComboMenu.AddLabel("Mark only 1 at once!");
+            ComboMenu.Add("comboERQW", new CheckBox("Use ERQW Combo", false));
+            ComboMenu.Add("comboQERW", new CheckBox("Use QERW Combo", false));
+            ComboMenu.Add("comboEQRW", new CheckBox("Use EQRW Combo", true));
+
+            HarassMenu = Menu.AddSubMenu("Harass", "lbharass");
+            HarassMenu.Add("hEQ", new CheckBox("Use EQ Harass", true));
+            HarassMenu.Add("hQW", new CheckBox("Use QW Harass", true));
+            HarassMenu.Add("hQEW", new CheckBox("Use QEW Harass", false));
 
         }
 
         public static void Game_OnDraw(EventArgs args)
         {
-            if (!DrawMenu["drawDisable"].Cast<CheckBox>().CurrentValue)
+            if (DrawMenu["drawQ"].Cast<CheckBox>().CurrentValue && !DrawMenu["drawDisable"].Cast<CheckBox>().CurrentValue && (Q.IsReady()))
             {
-                new Circle() { Color = Color.White, Radius = 700, BorderWidth = 2f }.Draw(ObjectManager.Player.Position);
+                Drawing.DrawCircle(_Player.Position, Q.Range, Color.Yellow);
             }
-        }  
-            public static void Game_OnUpdate(EventArgs args)
+
+            if (DrawMenu["drawW"].Cast<CheckBox>().CurrentValue && !DrawMenu["drawDisable"].Cast<CheckBox>().CurrentValue && (W.IsReady()))
+            {
+                Drawing.DrawCircle(_Player.Position, W.Range, Color.Yellow);
+            }
+
+            if (DrawMenu["drawE"].Cast<CheckBox>().CurrentValue && !DrawMenu["drawDisable"].Cast<CheckBox>().CurrentValue && (E.IsReady()))
+            {
+                Drawing.DrawCircle(_Player.Position, E.Range, Color.Yellow);
+            }
+
+            if (DrawMenu["drawQ"].Cast<CheckBox>().CurrentValue && !DrawMenu["drawDisable"].Cast<CheckBox>().CurrentValue && (!Q.IsReady()))
+            {
+                Drawing.DrawCircle(_Player.Position, Q.Range, Color.Red);
+            }
+
+            if (DrawMenu["drawW"].Cast<CheckBox>().CurrentValue && !DrawMenu["drawDisable"].Cast<CheckBox>().CurrentValue && (!W.IsReady()))
+            {
+                Drawing.DrawCircle(_Player.Position, W.Range, Color.Red);
+            }
+
+            if (DrawMenu["drawE"].Cast<CheckBox>().CurrentValue && !DrawMenu["drawDisable"].Cast<CheckBox>().CurrentValue && (!E.IsReady()))
+            {
+                Drawing.DrawCircle(_Player.Position, E.Range, System.Drawing.Color.Red);
+            }
+
+        }
+        public static void Game_OnUpdate(EventArgs args)
         {
-           var alvo = TargetSelector.GetTarget(720, DamageType.Magical);
+            var alvo = TargetSelector.GetTarget(720, DamageType.Magical);
 
             if (!alvo.IsValid()) return;
 
-            if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Combo)
+            if (ComboMenu["comboEQRW"].Cast<CheckBox>().CurrentValue && Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Combo)
             {
                 if (E.IsReady() && _Player.Distance(alvo) <= E.Range)
                 {
@@ -90,20 +136,80 @@ namespace KappaLeBlanc
                     R.Cast(alvo);
                 }
 
-                if (ObjectManager.Player.HasBuff("Distorion")) return;          
-
                 if (W.IsReady() && _Player.Distance(alvo) <= W.Range)
                 {
-                    W.Cast(alvo);   
+                    W.Cast(alvo);
+
                 }
 
 
-                
+            }
+
+            if (ComboMenu["comboQERW"].Cast<CheckBox>().CurrentValue && Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Combo)
+            {
+                if (Q.IsReady() && _Player.Distance(alvo) <= Q.Range)
+                {
+                    Q.Cast(alvo);
+                }
+
+                if (E.IsReady() && _Player.Distance(alvo) <= E.Range)
+                {
+                    E.Cast(alvo);
+                }
+
+                if (R.IsReady() && _Player.Distance(alvo) <= R.Range)
+                {
+                    R.Cast(alvo);
+                }
+
+                if (W.IsReady() && _Player.Distance(alvo) <= W.Range)
+                {
+                    W.Cast(alvo);
+
+                }
+
+
+            }
+
+            if (ComboMenu["comboERQW"].Cast<CheckBox>().CurrentValue && Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Combo)
+            {
+                if (E.IsReady() && _Player.Distance(alvo) <= E.Range)
+                {
+                    E.Cast(alvo);
+                }
+
+                if (R.IsReady() && _Player.Distance(alvo) <= R.Range)
+                {
+                    R.Cast(alvo);
+                }
+
+                if (Q.IsReady() && _Player.Distance(alvo) <= Q.Range)
+                {
+                    R.Cast(alvo);
+                }
+
+                if (W.IsReady() && _Player.Distance(alvo) <= W.Range)
+                {
+                    W.Cast(alvo);
+
+                }
+
+
             }
 
             if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Harass)
             {
-                if (Q.IsReady() && W.IsReady() && _Player.Distance(alvo) <= Q.Range)
+
+                if (HarassMenu["hQEW"].Cast<CheckBox>().CurrentValue && Q.IsReady() && W.IsReady() && _Player.Distance(alvo) <= W.Range + Q.Range)
+                {
+
+                    Q.Cast(alvo);
+                    E.Cast(alvo);
+                    W.Cast(alvo);
+
+                }
+
+                if (HarassMenu["hQW"].Cast<CheckBox>().CurrentValue && Q.IsReady() && W.IsReady() && _Player.Distance(alvo) <= Q.Range)
                 {
 
                     Q.Cast(alvo);
@@ -111,9 +217,18 @@ namespace KappaLeBlanc
                     W.Cast(alvo);
 
                 }
-                
+                if (HarassMenu["hEQ"].Cast<CheckBox>().CurrentValue && Q.IsReady() && W.IsReady() && _Player.Distance(alvo) <= E.Range)
+                {
+
+                    E.Cast(alvo);
+                    Q.Cast(alvo);
+
+                }
 
             }
+
         }
+        
+
     }
 }
