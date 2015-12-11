0263abec64f836a7bf4aeabfc9da650ca141bea3
Phandaros <phandarosgames@gmail.com>
2015-12-11 18:32:30 -0200

    LegendOfPoroKing

---
+++ LegendOfPoroKing/Poro.cs
@@ -0,0 +1,13 @@
+﻿using EloBuddy;
+using SharpDX;
+
+namespace LegendOfPoroKing
+{
+    public class Poro
+    {
+        public Vector3 start { get; set; }
+        public Vector3 end { get; set; }
+        public GameObject Obj { get; set; }
+        public int NetworkID { get; set; }
+    }
+}
